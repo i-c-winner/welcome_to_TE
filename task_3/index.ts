3a9f364c44771a8121c343ac9116e4feeba9e201
Belousov Dmitriy <d-belousov@hotmail.com>
2025-09-03 13:11:29 +0500

    feat: task3

--- task_3/index.ts
+++ task_3/index.ts
@@ -1,6 +1,6 @@
 interface BallonI {
-    id: number
-    isPublic: boolean
+	id: number;
+	isPublic: boolean;
 }
 
 /**
@@ -9,11 +9,13 @@ interface BallonI {
  * @returns {Number} количество шариков
  * @example const res = await fetchBallonAmount(202);
  */
-async function fetchBallonAmount(id: BallonI['id']): Promise<number> {
+async function fetchBallonAmount(id: BallonI["id"]): Promise<number> {
 	const RANDOM_TIMEOUT: number = Math.ceil(Math.random() * 10000); // 1-9 секунд
 	const RANDOM_AMOUNT: number = Math.ceil(Math.random() * id); // случайное число
 
-	return new Promise(resolve => setTimeout(() => resolve(RANDOM_AMOUNT), RANDOM_TIMEOUT));
+	return new Promise((resolve) =>
+		setTimeout(() => resolve(RANDOM_AMOUNT), RANDOM_TIMEOUT)
+	);
 }
 
 // данные о шариках
@@ -44,4 +46,23 @@ const BALLONS: { [key: string]: BallonI } = {
 	},
 };
 
-// Ваш код здесь
+// Тут полный ответ https://codesandbox.io/p/sandbox/task-3-forked-t9633z?file=%2Findex.ts%3A1%2C1-67%2C1
+
+/**
+ * В реальном React Componente нужно внести в useEffect что бы избежать излишних запросов на сервер
+ */
+const _ = Object.values(BALLONS).reduce((accum, currentValue) => {
+	if (currentValue.isPublic) {
+		accum.push(fetchBallonAmount(currentValue.id));
+	}
+	return accum;
+}, []);
+
+Promise.all(_)
+	.then((element) => {
+		const result = element.reduce((accum, value) => {
+			return accum + value;
+		}, 0);
+		console.log("Общее количество доступных шаров: ", result);
+	})
+	.then((error) => new Error(error));
